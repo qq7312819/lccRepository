8d066ecb8568318866f1768f0555403506527a33
lcc <lcc@lcc-PC>
2013-03-15 15:03:59 +0800

    submit one file

---
+++ test/src/com/example/test/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
